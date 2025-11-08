cd21d4bcf684c093cb0a768cd191bd17625b08ed
Aicha Lahnite <95276621+ranaitsan123@users.noreply.github.com>
2025-11-08 08:54:02 +0000

    initializing Pro Dev BE - Context Managers and Asynchronous programming in python - Project

---
+++ python-context-async-perations-0x02/0-databaseconnection.py
@@ -0,0 +1,25 @@
+import sqlite3
+
+class DatabaseConnection:
+    def __init__(self, db_name):
+        self.db_name = db_name
+        self.conn = None
+        self.cursor = None
+
+    def __enter__(self):
+        self.conn = sqlite3.connect(self.db_name)
+        self.cursor = self.conn.cursor()
+        return self.cursor
+
+    def __exit__(self, exc_type, exc_value, traceback):
+        if self.cursor:
+            self.cursor.close()
+        if self.conn:
+            self.conn.close()
+
+# Example usage
+if __name__ == "__main__":
+    with DatabaseConnection("users.db") as cursor:
+        cursor.execute("SELECT * FROM users;")
+        for row in cursor.fetchall():
+            print(row)
