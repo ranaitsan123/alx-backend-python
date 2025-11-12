fc71d8a851080aa6d777b344ef4c015ba35001b7
Aicha Lahnite <95276621+ranaitsan123@users.noreply.github.com>
2025-11-12 18:54:08 +0000

    updating PEP 8 compliant error

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -51,7 +51,10 @@ class TestMemoize(unittest.TestCase):
 
     def test_memoize(self):
         """Test that memoize caches method result"""
+
         class TestClass:
+            """Inner test class for memoization"""
+
             def a_method(self):
                 return 42
 
@@ -59,7 +62,9 @@ class TestMemoize(unittest.TestCase):
             def a_property(self):
                 return self.a_method()
 
-        with patch.object(TestClass, "a_method", return_value=42) as mock_method:
+        with patch.object(
+            TestClass, "a_method", return_value=42
+        ) as mock_method:
             obj = TestClass()
             self.assertEqual(obj.a_property, 42)
             self.assertEqual(obj.a_property, 42)
