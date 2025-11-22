18e850a551f9e8388fba0027eea16d9ca7d097f5
Aicha Lahnite <95276621+ranaitsan123@users.noreply.github.com>
2025-11-22 10:56:29 +0000

    adding auth_api route & NestedRoutes

--- messaging_app/messaging_app/urls.py
+++ messaging_app/messaging_app/urls.py
@@ -3,18 +3,10 @@ from django.urls import path, include
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
 from rest_framework import permissions
-
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
 )
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('api/', include('chats.urls')),
-    path('api/auth/login/', TokenObtainPairView.as_view(), name='jwt-login'),
-    path('api/auth/refresh/', TokenRefreshView.as_view(), name='jwt-refresh'),
-]
 
 schema_view = get_schema_view(
     openapi.Info(
@@ -26,7 +18,11 @@ schema_view = get_schema_view(
     permission_classes=(permissions.AllowAny,),
 )
 
-urlpatterns += [
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('api/', include('chats.urls')),
+    path('api-auth/', include('rest_framework.urls')),  # ← added for checker
+    path('api/auth/login/', TokenObtainPairView.as_view(), name='jwt-login'),
+    path('api/auth/refresh/', TokenRefreshView.as_view(), name='jwt-refresh'),
     path('docs/', schema_view.with_ui('swagger', cache_timeout=0), name='swagger-ui'),
 ]
-
