f2ee5670470b943e45b1d2132f75be3a9d2c322d
Aicha Lahnite <95276621+ranaitsan123@users.noreply.github.com>
2025-11-22 10:46:16 +0000

    updating - adding filters and status in views and urls

--- messaging_app/chats/urls.py
+++ messaging_app/chats/urls.py
@@ -1,15 +1,12 @@
-from rest_framework.routers import DefaultRouter
+from django.urls import path, include
+from rest_framework import routers
 from .views import ConversationViewSet, MessageViewSet
 
-from django.urls import path, include  # for checker
-from rest_framework import routers  # for checker
-
-# Dummy router
-dummy_router = routers.DefaultRouter()
-
-router = DefaultRouter()
+# Main router for the API
+router = routers.DefaultRouter()
 router.register(r'conversations', ConversationViewSet)
 router.register(r'messages', MessageViewSet)
 
-urlpatterns = router.urls
-
+urlpatterns = [
+    path('', include(router.urls)),
+]
