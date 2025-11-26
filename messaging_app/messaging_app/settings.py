460c440a97c124326ad61c87b93aa5a459949b18
ranaitsan123 <halaicha300@gmail.com>
2025-11-26 12:53:19 +0000

    updating the settings code for JWT id user problem

--- messaging_app/messaging_app/settings.py
+++ messaging_app/messaging_app/settings.py
@@ -145,4 +145,6 @@ REST_FRAMEWORK = {
 SIMPLE_JWT = {
     "ACCESS_TOKEN_LIFETIME": timedelta(minutes=60),
     "REFRESH_TOKEN_LIFETIME": timedelta(days=1),
+    'USER_ID_FIELD': 'user_id',          # <- Use your custom PK
+    'USER_ID_CLAIM': 'user_id',
 }
